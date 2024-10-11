a6e1a7ad645c445aa11327a1515bf42dd2b91419
Raivis Vitols <raivis.vitols@raivis.com>
2024-10-11 11:29:48 +0000

    Do not output any HTML from widgets is not enabled in config

--- Block/Widget/Product/Videos.php
+++ Block/Widget/Product/Videos.php
@@ -6,8 +6,43 @@ namespace Your\Integration\Block\Widget\Product;
 
 use Magento\Framework\View\Element\Template;
 use Magento\Widget\Block\BlockInterface;
+use Your\Integration\Model\System\Config;
 
 class Videos extends Template implements BlockInterface
 {
+    /**
+     * @var string
+     */
     protected $_template = 'Your_Integration::widget/product-videos.phtml';
+
+    /**
+     * @var Config
+     */
+    private Config $config;
+
+    /**
+     * @param Template\Context $context
+     * @param Config $config
+     * @param array $data
+     */
+    public function __construct(
+        Template\Context $context,
+        Config $config,
+        array $data = []
+    ) {
+        $this->config = $config;
+        parent::__construct($context, $data);
+    }
+
+    /**
+     * @return string
+     */
+    public function toHtml(): string
+    {
+        if (!$this->config->getIsConfigured()) {
+            return '';
+        }
+
+        return parent::toHtml();
+    }
 }
