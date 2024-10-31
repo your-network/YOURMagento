ff941b997da91b8bfad47a6f9aa471df01fe40ea
Raivis Vitols <raivis.vitols@raivis.com>
2024-10-31 12:36:50 +0000

    Added data install / revert patch

---
+++ Setup/Patch/Data/Install.php
@@ -0,0 +1,82 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Your\Integration\Setup\Patch\Data;
+
+use Magento\Framework\Exception\IntegrationException;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+use Magento\Framework\Setup\Patch\PatchRevertableInterface;
+use Your\Integration\Model\System\Config;
+use Your\Integration\Service\IntegrationManager;
+
+class Install implements DataPatchInterface, PatchRevertableInterface
+{
+    /**
+     * @var ModuleDataSetupInterface
+     */
+    private ModuleDataSetupInterface $moduleDataSetup;
+
+    /**
+     * @var IntegrationManager
+     */
+    private IntegrationManager $integrationManager;
+
+    /**
+     * @param ModuleDataSetupInterface $moduleDataSetup
+     * @param IntegrationManager $integrationManager
+     */
+    public function __construct(
+        ModuleDataSetupInterface $moduleDataSetup,
+        IntegrationManager $integrationManager
+    ) {
+        $this->moduleDataSetup = $moduleDataSetup;
+        $this->integrationManager = $integrationManager;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function apply(): self
+    {
+        return $this;
+    }
+
+    /**
+     * @return void
+     * @throws IntegrationException
+     */
+    public function revert(): void
+    {
+        $connection = $this->moduleDataSetup->getConnection();
+        $table = $this->moduleDataSetup->getTable('core_config_data');
+
+        $deletePath = Config::XML_PATH_CONTENT_LANGUAGE . '/%';
+        $connection->delete($table, ['path LIKE ?', $deletePath]);
+        $connection->delete($table, ['path IN (?)' => [
+            Config::XML_PATH_ENABLED,
+            Config::XML_PATH_API_KEY,
+            Config::XML_PATH_MPN_ATTRIBUTE_CODE,
+            Config::XML_PATH_GTIN_ATTRIBUTE_CODE,
+        ]]);
+
+        $this->integrationManager->deleteIntegration();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public static function getDependencies(): array
+    {
+        return [];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getAliases(): array
+    {
+        return [];
+    }
+}
